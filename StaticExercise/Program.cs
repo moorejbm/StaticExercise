d897fbb4393f5098f62d88c25dab24b313126da5
Jason Moore <moorejbm@gmail.com>
2021-02-04 11:58:40 -0600

    Update1

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celius = TempConverter.FahrenheitToCelsius(161.6);
+            var fahenheit = TempConverter.CelsiusToFahrenheit(72);
+
+            Console.WriteLine($"celcius {celius}");
+            Console.WriteLine($"Fahrenheit {fahenheit}");
+
+
+        }
+    }
+}
+ 
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahernheit)
+        {
+
+            var results =  (fahernheit - 32) / 1.8;
+
+            return results; 
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+
+            var results = (celsius * 9) / 5 + 32;
+
+            return results;
+        }
+    }
+}
